06259fe2496f4f3e7c5d1796d8a78dce0faf6e25
Swatto <58915441+Swatto86@users.noreply.github.com>
2024-12-01 21:56:47 +0000

    ChecksumUpdated

--- src/types/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'lucide-react' {
-  import { LucideIcon } from 'lucide-react';
-  export const ExternalLink: LucideIcon;
-  export const Github: LucideIcon;
-  export const Check: LucideIcon;
-  export const Copy: LucideIcon;
-  export const Circle: LucideIcon;
-} 
